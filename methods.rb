8716315b6b968b1054d6e7a9c8a9167266726805
tomer <tomertal52@yahoo.com>
2023-01-10 01:53:43 -0500

    commit tree bfb7bded3d3713147430fe759c206288dd3dcacb

--- methods.rb
+++ methods.rb
@@ -1,1 +1,29 @@
-# Your code here!
+def greet_programmer 
+    puts "Hello, programmer!";
+end
+greet_programmer;
+
+def greet(name)
+    puts "Hello, #{name}!";
+end
+greet("Tomer")
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!";
+end
+greet_with_default("Sunny")
+
+def add(num1, num2)
+    num1 + num2
+end
+pp add(10, 20)
+
+def halve(number)
+    if number.class != Integer
+        return nil
+    end
+
+    number / 2;
+end
+
+pp halve("t")
